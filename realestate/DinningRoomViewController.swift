05eb7322908cfa1378f04b77203edf1dbb78aa01
Min Aung Hein <minaunghein@gmail.com>
2016-10-27 20:47:42 +0630

    Initial Commit

---
+++ realestate/DinningRoomViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DinningRoomViewController.swift
+//  realestate
+//
+//  Created by Min Aung Hein on 27/10/16.
+//  Copyright © 2016 smag. All rights reserved.
+//
+
+import UIKit
+
+class DinningRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
