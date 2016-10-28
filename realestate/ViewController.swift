a38a8ac589637ffbc95732d9de1720f98d983088
Min Aung Hein <minaunghein@gmail.com>
2016-10-28 11:06:43 +0630

    Latest update to make new branch

--- realestate/ViewController.swift
+++ realestate/ViewController.swift
@@ -14,7 +14,17 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func gotoBedRoom(_ sender: AnyObject) {
+        
+        let brVC = BedRoomViewController(nibName: "BedRoomViewController", bundle: nil)
+        self.navigationController?.pushViewController(brVC, animated: true)
+    }
 
+    @IBAction func gotoDinning(_ sender: AnyObject) {
+        
+        let drVC = DinningRoomViewController(nibName: "BedRoomViewController", bundle: nil)
+        self.navigationController?.pushViewController(drVC, animated: true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
